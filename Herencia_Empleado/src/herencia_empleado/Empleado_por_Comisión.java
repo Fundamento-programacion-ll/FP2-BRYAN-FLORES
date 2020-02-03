d389d495a33114cd025b72d2f9aab516ed86b929
Bryan Flores <59964192+BryanFloresITQ@users.noreply.github.com>
2020-02-03 09:39:45 -0500

    Modificación Finalizado
    Java

--- Herencia_Empleado/src/herencia_empleado/Empleado_por_Comisión.java
+++ Herencia_Empleado/src/herencia_empleado/Empleado_por_Comisión.java
@@ -34,10 +34,10 @@ public class Empleado_por_Comisión extends Empleado{
     }
 
     public void setVentas_Brutas(double Ventas_Brutas) {
-        
-        if(Ventas_Brutas <= 0){this.Ventas_Brutas=0;}
-        else {this.Ventas_Brutas = Ventas_Brutas;}
+        this.Ventas_Brutas = Ventas_Brutas;
     }
+
+ 
 
     public void setTarifa_Comision(double Tarifa_Comision) {
         this.Tarifa_Comision = Tarifa_Comision;
@@ -49,6 +49,8 @@ public class Empleado_por_Comisión extends Empleado{
     public double Calcular_Ingresos() {
         
          if(Tarifa_Comision <= 0){this.Tarifa_Comision=0;}
+         
+         if(Ventas_Brutas <= 0){this.Ventas_Brutas=0;}
         
         return (this.Tarifa_Comision*this.Ventas_Brutas);
         
